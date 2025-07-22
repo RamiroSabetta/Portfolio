b3cf4cd49a486139b865a365f095450370a58c6f
Ramiro Sabetta <ramysabe080@gmail.com>
2025-07-22 18:36:38 +0000

    bueno, ahora quita esas || de los bordes, las letras hacelas mas pegadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"Source Code Pro"', 'monospace'],
-        headline: ['"Source Code Pro"', 'monospace'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Inter', 'sans-serif'],
         code: ['"Source Code Pro"', 'monospace'],
       },
       colors: {
