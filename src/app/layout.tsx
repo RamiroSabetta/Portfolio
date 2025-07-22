a93df9182c70e1937d62008b0faf15057cf188a3
Ramiro Sabetta <ramysabe080@gmail.com>
2025-07-22 20:10:01 +0000

    Perdon, Sabetta Ramiro - Portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Sabetta Ramiro- Portfolio',
+  title: 'Sabetta Ramiro - Portfolio',
   description: "Ramiro Sabetta's Portfolio",
 };
 
